97058074a038589139ee1b37fb3c52d46cc37bee
Bart Sokol <bartsokol@users.noreply.github.com>
2018-05-23 14:31:47 +0200

    Implement storage

--- src/FuelTracker.Api/Shared/Storage.cs
+++ src/FuelTracker.Api/Shared/Storage.cs
@@ -4,9 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Monacs.Core;
-using Monacs.Core.Async;
 using Monacs.Core.Unit;
 using Newtonsoft.Json;
+using static Monacs.Core.Result;
 
 namespace FuelTracker.Api.Shared
 {
@@ -24,33 +24,43 @@ namespace FuelTracker.Api.Shared
         private string FullPath => Path.Combine(_rootPath, $"{_fileName}.json");
 
         public async Task<Result<IEnumerable<T>>> GetAll() =>
-            await Monacs.Core.Async.Result.TryCatchAsync(
+            await AsyncResult.TryCatchAsync(
                 async () => File.Exists(FullPath) ? JsonConvert.DeserializeObject<IEnumerable<T>>(await File.ReadAllTextAsync(FullPath)) : Enumerable.Empty<T>(),
                 ex => Errors.Error(exception: ex));
 
-        public async Task<Result<T>> GetAsync(Guid id) =>
+        public async Task<Result<T>> Get(Guid id) =>
             await GetAll()
                 .BindAsync(r => r
                     .FirstOrDefault(item => item.Id == id)
                     .ToResult(() => Errors.Error($"Object of id {id} was not found!")));
 
-        public Task<Result<T>> Create(T newItem)
-        {
-            // TODO implement
-            return Task.FromResult(Monacs.Core.Result.Error<T>(Errors.Error()));
-        }
+        public async Task<Result<T>> Create(T newItem) =>
+            await GetAll()
+                .MapAsync(items => items.Append(newItem))
+                .BindAsync(StoreAll)
+                .MapAsync(_ => newItem);
 
-        public Task<Result<T>> Update(Guid id, T itemToUpdate)
-        {
-            // TODO implement
-            return Task.FromResult(Monacs.Core.Result.Error<T>(Errors.Error()));
-        }
+        public async Task<Result<T>> Update(Guid id, T itemToUpdate) =>
+            await GetAll()
+                .MapAsync(items => items.Where(x => x.Id != id).Append(itemToUpdate))
+                .BindAsync(StoreAll)
+                .MapAsync(_ => itemToUpdate);
 
-        public Task<Result<Unit>> Delete(Guid id)
-        {
-            // TODO implement
-            return Task.FromResult(Monacs.Core.Unit.Result.Ok());
-        }
+        public async Task<Result<Unit>> Delete(Guid id) =>
+            await GetAll()
+                .MapAsync(items => items.Where(x => x.Id != id))
+                .BindAsync(StoreAll);
+
+        private async Task<Result<Unit>> StoreAll(IEnumerable<T> items) =>
+            await AsyncResult.TryCatchAsync(
+                async () => {
+                    var dir = Path.GetDirectoryName(FullPath);
+                    if (!Directory.Exists(dir))
+                        Directory.CreateDirectory(dir);
+                    await File.WriteAllTextAsync(FullPath, JsonConvert.SerializeObject(items));
+                    return Unit.Default;
+                },
+                ex => Errors.Error(exception: ex));
     }
 
     public class CarStorage : FileStorage<Cars.Car>
